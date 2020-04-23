5faa6a0fbbc79ff65210849735bfbee41d17f9cb
Keelan <keelanderrick@gmail.com>
2020-04-23 04:51:42 -0500

    added a wip function to update mongo database

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-import React, {Component} from 'react';
+import React, {
+  Component
+} from 'react';
 import LadderSelector from './components/ladderSelector';
 import LadderEntry from './components/ladderEntry';
 import CharacterPanel from './components/characterPanel';
+import {
+  Button
+} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import fetch from 'node-fetch';
 
 class App extends Component {
   constructor(props) {
@@ -12,10 +18,11 @@ class App extends Component {
     this.onCloseCharacterPanel = this.onCloseCharacterPanel.bind(this);
   }
 
-  render () {
+  render() {
     return (
       <div>
         <LadderSelector ladders={this.state.ladders} onLadderChange={this.handleLadderChange} />
+        <Button variant="warning" onClick={this.importData.bind(this)}> Import Data </Button>
         <LadderEntry selectedLadder={this.state.selectedLadder} entries={this.state.entries} onCharacterSelect={this.handleCharacterSelect} />
         <CharacterPanel onClose={this.onCloseCharacterPanel} accountName={this.state.selectedAccountName} characterName={this.state.selectedCharacter} />
       </div>
@@ -30,39 +37,57 @@ class App extends Component {
     selectedAccountName: ''
   }
 
-  componentDidMount () {
+  importData() {
+    fetch('/api/import')
+  }
+
+  componentDidMount() {
     fetch('/api/ladders')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ladders: data})
-    })
-    .catch(console.log)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          ladders: data
+        })
+      })
+      .catch(console.log)
 
     fetch(`/api/ladders/${this.state.selectedLadder}`)
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({entries: data.entries})
-    })
-    .catch(console.log)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          entries: data.entries
+        })
+      })
+      .catch(console.log)
   }
 
-  handleLadderChange (newId) {
+  handleLadderChange(newId) {
     fetch(`/api/ladders/${newId}`)
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({entries: data.entries})
-    })
-    .catch(console.log)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          entries: data.entries
+        })
+      })
+      .catch(console.log)
 
-    this.setState({selectedLadder: newId});
+    this.setState({
+      selectedLadder: newId
+    });
   }
 
-  handleCharacterSelect (charName, accName) {
-    this.setState({selectedCharacter: charName, selectedAccountName: accName});
+  handleCharacterSelect(charName, accName) {
+    this.setState({
+      selectedCharacter: charName,
+      selectedAccountName: accName
+    });
   }
 
-  onCloseCharacterPanel (e) {
-    this.setState({selectedCharacter: '', selectedAccountName: ''});
+  onCloseCharacterPanel(e) {
+    this.setState({
+      selectedCharacter: '',
+      selectedAccountName: ''
+    });
   }
 }
 
--- index.js
+++ index.js
@@ -1,57 +1,93 @@
 const express = require('express');
+const mongodb = require('mongodb');
 const fetch = require('node-fetch');
+const URI = require('urijs');
 const path = require('path');
 
 const app = express();
+const mongo = mongodb.MongoClient;
+var dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-tht63.gcp.mongodb.net/data?retryWrites=true://localhost:27017/data`
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('/api/ladders', (req, res) =>  { 
-    fetch('https://api.pathofexile.com/leagues')
-    .then(response => {
-      response.json().then(json => {
-        res.json(json)
-      })
+mongo.connect(dbUrl, {
+        useUnifiedTopology: true
     })
-    .catch(error => {
-      console.log(error)
-    })
-});
+    .then(client => {
+        const db = client.db('')
+        const charactersCollection = db.collection('characters')
 
-app.get('/api/ladders/:ladder', (req, res) => {
-    let ladder = req.params.ladder.trim()
-    console.log(ladder);
-    fetch(`https://api.pathofexile.com/ladders/${ladder}`)
-    .then(response => {
-        response.json().then(json => {
-            res.json(json)
+        // Serve static files from the React app
+        app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+
+        app.get('/api/import', async (req, res) => {
+            try {
+                const response = await fetch(`https://api.pathofexile.com/ladders/SSF Delirium`)
+                const data = await response.json()
+                for (i in data.entries) {
+                    var characterName = data.entries[i].character.name;
+                    var accountName = data.entries[i].account.name;
+                    let windowUri = new URI(`https://www.pathofexile.com/character-window/get-items?character=${characterName}&accountName=${accountName}`)
+                    console.log(encodeURI(windowUri.toString()))
+                    const response = await fetch(encodeURI(windowUri.toString()))
+                    const items = await response.json()
+                    if (items.items)
+                        data.entries[i].items = items.items
+                    data.entries[i]._id = data.entries[i].character.id;
+                }
+                charactersCollection.insertMany(data.entries)
+                res.json("Success");
+            } catch (error) {
+                console.log(error);
+            }
+        });
+
+        // api endpoint for league list
+        app.get('/api/ladders', async (req, res) => {
+            try {
+                const response = await fetch('https://api.pathofexile.com/leagues')
+                const data = await response.json()
+                res.json(data);
+            } catch (error) {
+                console.log(error);
+            }
+        });
+
+        // endpoint for ladder of a league
+        app.get('/api/ladders/:ladder', async (req, res) => {
+            try {
+                let ladder = req.params.ladder.trim()
+                const response = await fetch(`https://api.pathofexile.com/ladders/${ladder}`)
+                const data = await response.json()
+                res.json(data);
+            } catch (error) {
+                console.log(error);
+            }
+
         })
-    })
-    .catch(error => {
-        console.log(error)
-    })
-})
 
-app.get('/api/get-items/:account/:character', (req, res) => {
-    let account = req.params.account.trim()
-    let character = req.params.character.trim()
-    fetch(`https://www.pathofexile.com/character-window/get-items?character=${character}&accountName=${account}`)
-    .then(response => {
-        response.json().then(json => {
-            res.json(json)
+        // endpoint for specific character information
+        app.get('/api/get-items/:account/:character', async (req, res) => {
+            try {
+                let account = req.params.account.trim()
+                let character = req.params.character.trim()
+                const response = await fetch(`https://www.pathofexile.com/character-window/get-items?character=${character}&accountName=${account}`)
+                const data = await response.json()
+                res.json(data);
+            } catch (error) {
+                console.log(error);
+            }
         })
-    })
-    .catch(error => {
-        console.log(error)
-    })
-})
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+        // The "catchall" handler: for any request that doesn't
+        // match one above, send back React's index.html file.
+        app.get('*', (req, res) => {
+            console.log(req);
+            res.sendFile(path.join(__dirname + '/client/build/index.html'));
+        });
+
+    })
+    .catch(console.error);
 
 const port = process.env.PORT || 6000;
 app.listen(port);
